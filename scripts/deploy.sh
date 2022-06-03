860293d41c47c4b175c3a969cc4eb33ae8ee75c9
Tim Daubenschütz <tim@daubenschuetz.de>
2022-06-03 16:54:36 +0200

    Confirm Rinkeby RPC endpoint via chainid

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -5,6 +5,9 @@ echo Enter Your Mainnet RPC URL:
 echo Example: "https://eth-mainnet.alchemyapi.io/v2/XXXXXXXXXX"
 read -s rpc
 
+curl -s -X POST --data '{"jsonrpc":"2.0","method":"net_version","params":[],"id":67}' $rpc | \
+grep -q '\"result\":\"4\"' && echo "Confirming Rinkeby RPC Endpoint via chainId" || exit 1;
+
 # Read the contract name
 echo Which contract do you want to deploy \(eg Greeter\)?
 read contract
